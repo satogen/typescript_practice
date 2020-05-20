dd417508afb8eaca06ed247b801bf13283b614c9
佐藤現 <satougen@satougennoMacBook-Pro.local>
2020-05-20 11:33:04 +0900

    const add file

---
+++ src/as-const.ts
@@ -0,0 +1,16 @@
+export {};
+
+let name = 'atsushi';
+
+name = 'ham';
+
+let nicname = 'Ham' as const;
+
+// nicname = 'hamtaro';
+
+let profile = {
+  name: 'stushi',
+  height: 178,
+} as const;
+
+// profile.name = 'stushi';
